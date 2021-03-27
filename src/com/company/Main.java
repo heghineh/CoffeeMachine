8e35cb9dc52cf10753008ea85ebaff96a6b46223
User <heghine_aghajanyan@mail.ru>
2021-03-27 03:15:51 -0700

    Coffee machine problem (+ exception changes)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,12 +5,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your choice!");
-        Command command = new Command(sc.nextLine());
-        CoffeeMachine coffeeMachine = new CoffeeMachine();
-        Drink drink = coffeeMachine.makeDrink(command);
+        while (true) {
+            try {
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter your choice!");
+                Command command = new Command(sc.nextLine());
+                CoffeeMachine coffeeMachine = new CoffeeMachine();
+                Drink drink = coffeeMachine.makeDrink(command);
 
-        System.out.println(drink);
+                System.out.println(drink);
+                break;
+            } catch (NullPointerException exception) {
+                System.out.println("You can choose only tea or coffee, please try again!\"!");
+            } catch (ArrayIndexOutOfBoundsException exception) {
+                System.out.println("Invalid expression, please try again.");
+            } catch (NumberFormatException exception){
+                System.out.println("Invalid expression, please try again.");
+            }
+        }
     }
 }
